60ac1c5f348d6a0dd0a92c03562de2a11582b1ca
smiths0n <alex@smiths0n.co.uk>
2014-12-03 11:18:22 +0000

    Update models.py
    correct one now?

--- iospush/models.py
+++ iospush/models.py
@@ -152,7 +152,7 @@ class Device(models.Model):
             if custom_cert is None:
                 custom_cert = self._getApnCertPath()
             c = ssl.wrap_socket(s,
-                                ssl_version=ssl.TLSv1_METHOD,
+                                ssl_version=ssl.PROTOCOL_TLSv1,
                                 certfile=custom_cert)
             c.connect((self._getApnHostName(), 2195))
 
@@ -262,7 +262,7 @@ def doFeedbackLoop(sandbox = False):
 
     s = socket()
     c = ssl.wrap_socket(s,
-                        ssl_version=ssl.TLSv1_METHOD,
+                        ssl_version=ssl.PROTOCOL_TLSv1,
                         certfile=settings.APN_LIVE_PUSH_CERT)
     c.connect((settings.APN_FEEDBACK_HOST, 2196))
 
